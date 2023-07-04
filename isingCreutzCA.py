697683af45fefd30dc11b61d5e8119029819740b
unknown <sina.abdollahi@gmail.com>
2023-07-04 03:32:03 +0200

    working-ish ising creutz ca

--- isingCreutzCA.py
+++ isingCreutzCA.py
@@ -6,10 +6,10 @@ import torch.nn.functional as F
 
 
 class Rule(nn.Module):
-    def __init__(self, BETA=1, RADIUS=2):
+    def __init__(self, HK_MAX, RADIUS=2):
         super().__init__()
-        self.beta = BETA
         self.radius = RADIUS
+        self.Hk_max = HK_MAX
         Rk = 2*RADIUS + 1
 
         nearest_neighbours = torch.zeros(1, 1, Rk, Rk).cuda()
@@ -22,38 +22,41 @@ class Rule(nn.Module):
     def forward(self, x):
 
         Rk = self.radius
-        s = F.pad(x[:, 0, ...], (Rk, Rk, Rk, Rk), mode='circular')
-        momentum = F.pad(x[:, 1:, ...], (Rk, Rk, Rk, Rk), mode='circular')
-        Js = F.conv2d(s, self.nearest_neighbours, padding=0)
-        Hk = 4 * (momentum[0, 0] + 2 * momentum[0, 1])  # the 4 is to match this with the number of neighbours
-        delta_e = 2 * x * Js
+        s = x[:, [0], ...]
+        s_pad = F.pad(s, (Rk, Rk, Rk, Rk), mode='circular')
+        momentum = x[:, [1], ...]
+        Js = F.conv2d(s_pad, self.nearest_neighbours, padding=0)
+        delta_e = 2 * s * Js
 
-        (12 - Hk) - delta_e
+        Hk_new = momentum - delta_e
+        conserve_flag = torch.logical_and(Hk_new >= 0., Hk_new <= self.Hk_max)
+        dropout_mask = (torch.rand_like(x[0, 0]) > 0.75).unsqueeze(0).unsqueeze(0)
 
-        definite_flip = delta_e <= 0
-        p = torch.exp(-delta_e * self.beta)
-        p = torch.where(definite_flip, torch.ones_like(x), p)
+        flip_flag = torch.logical_and(conserve_flag, dropout_mask)
+        flip_spin = -2. * flip_flag + 1
 
-        rand = torch.rand_like(x)
+        s_new = x[:, [0], ...] * flip_spin
+        momentum = torch.where(flip_flag, Hk_new, momentum)
 
-        dropout_mask = (torch.rand_like(x[0, 0]) > 0.5).unsqueeze(0).unsqueeze(0)
-        flip = -2. * torch.logical_and(rand < p, dropout_mask) + 1
-
-        return (x * flip)
+        return torch.cat([s_new, momentum], axis=1)
 
 class isingCA(nn.Module):
-    def __init__(self, BETA=1, RADIUS=1):
+    def __init__(self, HK_MAX, RADIUS=1):
         super().__init__()
         self.radius = RADIUS
 
-        self.rule = Rule(BETA, RADIUS)
+        self.rule = Rule(HK_MAX, RADIUS)
 
-    def initGrid(self, shape):
-        rand = (np.random.rand(1, 3, shape[0], shape[1]) > 0.5) * 2. - 1.
-        return torch.cuda.FloatTensor(rand)
+    def initGrid(self, shape, init_order=None, init_energy=None):
+        if init_energy is None:
+            init_energy = self.rule.Hk_max
+        if init_order is None:
+            init_order = 0.5
+
+        rand_spin = (torch.rand(1, 1, shape[0], shape[1]) > init_order) * 2. - 1.
+        rand_momentum = torch.rand_like(rand_spin) * np.clip(init_energy, None, self.rule.Hk_max)
+
+        return torch.cat([rand_spin, rand_momentum], axis=1).cuda()
 
     def forward(self, x):
         return self.rule(x)
-
-    def cleanup(self):
-        del self.psi
