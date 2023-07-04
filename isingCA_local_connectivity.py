bd1ddcca3466d8aa2e7a81cdba92f81216e843c4
Heysoos <sina.abdollahi@gmail.com>
2023-07-05 00:30:33 +0200

    seems to do SoC, pretty pygame visualizations

--- isingCA_local_connectivity.py
+++ isingCA_local_connectivity.py
@@ -13,10 +13,12 @@ class Rule(nn.Module):
 
         self.J_adapt = False
         self.adapt_lr = 1.
-        self.alpha = 0.5
+        self.alpha = 0.9
         self.h = 0.001
-        self.eps = 0.01
+        self.eps = 0.002
         self.max_weight = 4.
+
+        self.trace_memory = 0.995
 
         self.numel = RES[0] * RES[1]
 
@@ -28,14 +30,15 @@ class Rule(nn.Module):
         null = torch.zeros_like(rm).cuda()
         self.rm = torch.where(condition, exp_rm, null).unsqueeze(0).unsqueeze(-1)
 
-        # nearest_neighbours = torch.zeros(1, Rk, Rk, self.numel).cuda()
-        nearest_neighbours = torch.rand(1, Rk, Rk, self.numel).cuda()
+        nearest_neighbours = torch.zeros(1, Rk, Rk, self.numel).cuda()
+        # nearest_neighbours = torch.rand(1, Rk, Rk, self.numel).cuda()
 
-        # nearest_neighbours[:, RADIUS, :, :] = 1.
-        # nearest_neighbours[:, :, RADIUS, :] = 1.
+        nearest_neighbours[:, RADIUS, :, :] = 1.
+        nearest_neighbours[:, :, RADIUS, :] = 1.
         nearest_neighbours[:, RADIUS, RADIUS, :] = 0
 
-        self.nearest_neighbours = (nearest_neighbours * self.rm).reshape(1, -1, self.numel)
+        # self.nearest_neighbours = (nearest_neighbours * self.rm).reshape(1, -1, self.numel)
+        self.nearest_neighbours = 0.1 * (nearest_neighbours).reshape(1, -1, self.numel)
 
 
     def forward(self, x):
@@ -62,14 +65,14 @@ class Rule(nn.Module):
 
         if self.J_adapt and torch.rand(1) > 0.95:
             s_i = F.unfold(tr, 1)
-            # s_j = F.unfold(F.pad(tr, (Rk, Rk, Rk, Rk), mode='circular'), 2*Rk+1)
-            # m = s_j.mean(dim=1)
-            m = s_i.mean(dim=1)
+            s_j = F.unfold(F.pad(tr, (Rk, Rk, Rk, Rk), mode='circular'), 2*Rk+1)
+            # m = s_j.mean(dim=1, keepdim=True)
+            # m = s_i.mean(dim=1)
             # dJ_hebb = self.h * (s_i * s_j) / (4 * (Rk + 1))  # hebbian growth
             # noisy_L1 = self.eps / (4 * (Rk + 1)) * self.nearest_neighbours # L1 decay, scales with magnetization
             # dJ = dJ_hebb - noisy_L1 * m ** 2
 
-            dJ = (self.h - self.eps * m.abs() ) * self.rm.reshape(1, -1, 1)
+            dJ = (self.h - self.eps * s_j.abs() * s_i.abs()) # * self.rm.reshape(1, -1, 1)
 
             new_J = self.nearest_neighbours + \
                     dJ * self.adapt_lr # * torch.rand_like(self.nearest_neighbours) # stochastic dJ
@@ -81,7 +84,7 @@ class Rule(nn.Module):
 
             self.nearest_neighbours = ((1 - self.alpha) * self.nearest_neighbours + self.alpha * new_J).clip(min=0.)
 
-        tr = 0.9 * tr + 0.1 * s
+        tr = self.trace_memory * tr + (1 - self.trace_memory) * s
         return torch.cat([(s * flip), tr, b], axis=1)
 
 class isingCA(nn.Module):
