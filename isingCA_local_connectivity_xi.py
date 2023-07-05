56f57c10dedfafe90af660addfa708e7ce73c722
Heysoos <sina.abdollahi@gmail.com>
2023-07-05 22:40:00 +0200

    more SoC stuff, using negative weights, measuring criticality

--- isingCA_local_connectivity_xi.py
+++ isingCA_local_connectivity_xi.py
@@ -20,6 +20,9 @@ class Rule(nn.Module):
 
         self.trace_memory = 0.995
 
+        self.num_obvs = 4
+        self.obv_decay = 0.95
+
         self.numel = RES[0] * RES[1]
 
         # radial kernel
@@ -30,18 +33,18 @@ class Rule(nn.Module):
         null = torch.zeros_like(rm).cuda()
         self.rm = torch.where(condition, exp_rm, null).unsqueeze(0).unsqueeze(-1)
 
-        nearest_neighbours = torch.randn(1, Rk, Rk, self.numel).cuda()
-        # nearest_neighbours = torch.zeros(1, Rk, Rk, self.numel).cuda()/ RADIUS ** 2
+        # nearest_neighbours = torch.randn(1, Rk, Rk, self.numel).cuda()
+        nearest_neighbours = torch.zeros(1, Rk, Rk, self.numel).cuda()/ RADIUS ** 2
 
-        # nearest_neighbours[:, RADIUS, :, :] = 1.
-        # nearest_neighbours[:, :, RADIUS, :] = 1.
+        nearest_neighbours[:, RADIUS, :, :] = 1.
+        nearest_neighbours[:, :, RADIUS, :] = 1.
         nearest_neighbours[:, RADIUS, RADIUS, :] = 0
 
         # self.nearest_neighbours = (nearest_neighbours * self.rm).reshape(1, -1, self.numel)
         self.nearest_neighbours = 1 * (nearest_neighbours).reshape(1, -1, self.numel)
 
 
-    def forward(self, x):
+    def forward(self, x, make_obv=False):
 
         s = x[:, [0], ...]
         tr = x[:, [1], ...]
@@ -51,8 +54,24 @@ class Rule(nn.Module):
 
         s_pad = F.pad(s, (Rk, Rk, Rk, Rk), mode='circular')
 
+        sJs = 2 * F.unfold(s, 1) * (F.unfold(s_pad, 2 * Rk + 1) * self.nearest_neighbours)
+        # Js = F.conv2d(s_pad, self.nearest_neighbours, padding=0)
+        delta_e = sJs.sum(dim=1).view(*shape)
+        E = (-0.5 * delta_e)
+
         Js = (F.unfold(s_pad, 2*Rk+1) * self.nearest_neighbours).sum(dim=1).view(*shape)
         delta_e = 2 * s * Js
+
+        if make_obv:
+            ### OBSERVATIONS ###
+            e = E.mean() / 4.
+            e2 = e**2
+
+            m = s.mean()
+            m2 = m ** 2
+
+            obvs = torch.stack([e, e2, m, m2], axis=0)
+            #####################
 
         definite_flip = delta_e <= 0
         p = torch.exp(-delta_e * b)
@@ -60,42 +79,38 @@ class Rule(nn.Module):
 
         rand = torch.rand_like(s)
 
-        dropout_mask = (torch.rand_like(s[0, 0]) > 0.9).unsqueeze(0).unsqueeze(0)
+        dropout_mask = (torch.rand_like(s[0, 0]) > 0.5).unsqueeze(0).unsqueeze(0)
         flip = -2. * torch.logical_and(rand < p, dropout_mask) + 1
 
         if self.J_adapt and torch.rand(1) > 0.:
             s_i = F.unfold(tr, 1)
             s_j = F.unfold(F.pad(tr, (Rk, Rk, Rk, Rk), mode='circular'), 2*Rk+1)
-            # m = s_j.mean(dim=1, keepdim=True)
-            # m = s_i.mean(dim=1)
-            # dJ_hebb = self.h * (s_i * s_j) / (4 * (Rk + 1))  # hebbian growth
-            # noisy_L1 = self.eps / (4 * (Rk + 1)) * self.nearest_neighbours # L1 decay, scales with magnetization
-            # dJ = dJ_hebb - noisy_L1 * m ** 2
+
 
             growth = self.h / Rk  # correlate
-            # decay = self.eps * (s_j * s_i).clamp(min=0.)
             # decay = self.eps * (s_j.mean(dim=1, keepdim=True) * s_i)  # decorrelate if mag.
             decay = self.eps * (s_j * s_i)  # decorrelate if mag.
-            dJ = (growth - decay) # * self.rm.reshape(1, -1, 1)
+            dJ = (growth - decay)  # * self.rm.reshape(1, -1, 1)
 
             new_J = self.nearest_neighbours + \
                     dJ * self.adapt_lr * (torch.rand_like(self.nearest_neighbours) > 0.5) # stochastic dJ
             new_J[:, Rk * (2 * Rk + 1) + Rk, :] = 0.  # set center pixel to 0.
 
             # set corners to 0
-            # new_J[:, 0, :] = 0.
-            # new_J[:, 2*Rk, :] = 0.
-            # new_J[:, -(2*Rk+1), :] = 0.
-            # new_J[:, -1, :] = 0.
-
-            # new_J_sum = new_J.abs().sum(dim=1, keepdim=True)
-            # new_J_sum *= (1 + 0.1 * torch.randn_like(new_J_sum))  # noisy normalization
-            # new_J = new_J * (self.max_weight / new_J_sum)
+            new_J[:, 0, :] = 0.
+            new_J[:, 2*Rk, :] = 0.
+            new_J[:, -(2*Rk+1), :] = 0.
+            new_J[:, -1, :] = 0.
 
             self.nearest_neighbours = ((1 - self.alpha) * self.nearest_neighbours + self.alpha * new_J)
 
         tr = self.trace_memory * tr + (1 - self.trace_memory) * s
-        return torch.cat([(s * flip), tr, b], axis=1)
+
+        if make_obv:
+            return torch.cat([(s * flip), tr, b], axis=1), obvs
+        else:
+            return torch.cat([(s * flip), tr, b], axis=1)
+
 
 class isingCA(nn.Module):
     def __init__(self, RES, BETA=1, RADIUS=2):
@@ -105,11 +120,11 @@ class isingCA(nn.Module):
 
     def initGrid(self, unfold=True):
         shape = self.rule.res
-        rand = (torch.rand(1, 3, shape[0], shape[1]) > torch.rand(1)) * 2. - 1.
+        rand = (torch.rand(1, 3, shape[0], shape[1]) > 0.5) * 2. - 1.
         rand[:, 1, ...] = torch.zeros_like(rand[:, 1, ...])
         rand[:, -1, ...] = torch.ones_like(rand[:, -1, ...]) * self.rule.beta
 
         return rand.cuda()
 
-    def forward(self, x):
-        return self.rule(x)
+    def forward(self, x, make_obv=False):
+        return self.rule(x, make_obv)
